45c453974cd4d5a850978c375eb3322a1e35e8b1
Professor Choren <anderson.choren@faqi.edu.br>
2024-10-04 12:10:54 -0300

    Material

---
+++ loops/script.js
@@ -0,0 +1,19 @@
+const palindromes = [
+  "arara",
+  "joão",
+  "mirim",
+  "radar",
+  "flutter",
+  "ana",
+  "javascript",
+  "civic",
+  "dart",
+];
+
+// Forma com Loop Do/While
+
+
+// Forma com Loop While
+
+
+// Forma com Loop For
